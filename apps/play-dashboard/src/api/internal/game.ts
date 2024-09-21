0a26c726d3db7c4818f659116a81cdc776229714
Williams Sissoko <35783824+wcisco17@users.noreply.github.com>
2024-09-21 19:57:49 +0700

    bugs(THOR-00): fix prisma issue remove unused code

--- apps/play-dashboard/src/api/internal/game.ts
+++ apps/play-dashboard/src/api/internal/game.ts
@@ -1,27 +1,11 @@
 import {prisma} from "@/utils/prisma/client";
-import {type Game} from '@thor/db'
 import {handlePrismaError} from "@/utils/prisma/error";
 import {createPrismaApiResult, type PrismaCustomAPIResult} from "@/utils/prisma/result";
-
-export const fetchGame = async (gameId: string): Promise<PrismaCustomAPIResult<Game | Pick<Game, 'prompt' | 'points'>>> => {
-  try {
-    const game = await prisma.game.findUnique({
-      where: {id: gameId},
-      select: {prompt: true, points: true}
-    });
-    if (!game) {
-      return createPrismaApiResult(false, undefined, {error: 'Game not found', status: 404});
-    }
-    return createPrismaApiResult(true, game);
-  } catch (error) {
-    return createPrismaApiResult(false, undefined, handlePrismaError(error));
-  }
-};
 
 export const updateGameStatus = async (gameId: string, winnerId: string | null): Promise<PrismaCustomAPIResult<void>> => {
   try {
     await prisma.game.update({
-      where: { id: gameId },
+      where: {id: gameId},
       data: {
         status: 'COMPLETED',
         winnerId
